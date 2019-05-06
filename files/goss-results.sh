62f570ac080670915ea720b2657c9c75240820fb
Dos Barbos <evgenii.dos@li9.com>
2019-05-06 11:51:08 -0400

    go

--- files/goss-results.sh
+++ files/goss-results.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > results.yml
-/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > results-for-mail.yml
+touch /root/results.yml
+touch /root/results-for-mail.yml
+goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results.yml
+goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results-for-mail.yml
+
+if [ $? -eq 0 ]
+then
+  echo "Success: zaebis."
+  exit 0
+else
+  echo "Failure: nezaebis" >&2
+  exit 1
+fi
+
 exit 0
