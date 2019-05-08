ff9ab60a1e94d4c69ff4e84b121ce476eaed8222
Dos Barbos <evgenii.dos@li9.com>
2019-05-07 21:06:41 -0400

    go

--- files/goss-results.sh
+++ files/goss-results.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-touch /root/results.yml
-touch /root/results-for-mail.yml
 
 /usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results.txt
 /usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results-for-mail.txt
