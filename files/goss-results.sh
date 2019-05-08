8c6feaaf2685e58f6c6f4753704659424cbfd866
Dos Barbos <evgenii.dos@li9.com>
2019-05-07 23:54:14 -0400

    go

--- files/goss-results.sh
+++ files/goss-results.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 |egrep -v "ssh-access: exists: matches|exit-status: matches expectation" > /root/results.txt
+/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 |egrep -v "ssh-access: exists: matches|exit-status: matches expectation"|sed 1d > /root/results.txt
 
+echo "" >> /root/results.txt
 echo "The common results are:" >> /root/results.txt
 echo "" >> /root/results.txt
-cat /root/results.txt|awk '{if ($1=="not") notok=notok+1; if ($1=="ok") ok=ok+1} END{print "ok=", ok, "notok=",notok}' >> /root/results.txt
+
+cat /root/results.txt|awk '{if ($1=="not"||$0~/SKIP/) failed=failed+1; else if ($1=="ok") ok=ok+1} END{print "ok=", ok, "failed=",failed}' >> /root/results.txt
 
 exit 0
