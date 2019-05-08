73728a384b69444bd56d98fd0c8fbbcc1d7b2c2a
Dos Barbos <evgenii.dos@li9.com>
2019-05-07 22:27:11 -0400

    go

--- files/goss-results.sh
+++ files/goss-results.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results.txt
-/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 > /root/results-for-mail.txt
+/usr/local/bin/goss -g /root/goss.yaml validate --format tap --max-concurrent 1 |egrep -v "ssh-access: exists: matches|exit-status: matches expectation" > /root/results.txt
+
+echo "The common results are:" >> /root/results.txt
+echo "" >> /root/results.txt
+cat /root/results.txt|awk '{if ($1=="not") notok=notok+1; if ($1=="ok") ok=ok+1} END{print "ok=", ok, "notok=",notok}' >> /root/results.txt
 
 exit 0
--- files/goss-ssh-results.sh
+++ files/goss-ssh-results.sh
@@ -2,4 +2,10 @@
 
 ssh -o BatchMode=yes -o StrictHostKeyChecking=no sshuser@localhost "id" > /root/results-ssh-access
 
+if [[ $? -ne 0 ]]; then
+  rm -rf /root/results-ssh-access
+else
+  echo "ok"
+fi
+
 exit 0
